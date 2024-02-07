c066b06a709bce1b12dcaef0bc518799b577a001
Kumar Nitesh Singh <71548598+imnikku@users.noreply.github.com>
2024-02-07 09:19:33 +0530

    Delete build directory

--- build/model/Link.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LinkModel = exports.LinkSchema = void 0;
-const mongoose_1 = require("mongoose");
-exports.LinkSchema = new mongoose_1.Schema({
-    title: String,
-    url: String,
-}, { timestamps: true });
-exports.LinkModel = (0, mongoose_1.model)('link', exports.LinkSchema);
